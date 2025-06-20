2d2e367e3d62531026b5b3b6a1ab209716c0d842
unknown <elotgodsplan2@gmail.com>
2025-06-21 00:20:36 +0100

    Handling event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,15 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+   
+
 
   return (
     <>
     <h1>List</h1>
-     {items.length === 0 && <p>No item found</p> }
+      {items.length === 0 && <p>No item found</p> }
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item}>{item}</li>
         ))}
       </ul>
     </>
