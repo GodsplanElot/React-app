df2b06025ad6380ea3718e02c0e5e5ed6d2ca6fe
unknown <elotgodsplan2@gmail.com>
2025-06-23 17:51:16 +0100

    Fix Active class (made Dynamic)

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,18 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  //Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  let selectedIndex = 0;
+  useState
    
-
 
   return (
     <>
     <h1>List</h1>
       {items.length === 0 && <p>No item found</p> }
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item active" key={item} onClick={handleClick} >{item}</li>
+        {items.map((item, index) => (
+          <li className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={ () => {selectedIndex = index; }} >{item}</li>
         ))}
       </ul>
     </>
