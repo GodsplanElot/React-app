b16c3c19770bad196e8a8312eaf624e7cd5fd2cc
Godsplan Elot <elotgodsplan2@gmail.com>
2025-08-24 08:15:13 +0100

    Finish Fetching users and useri-ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+ const [users, setUsers] = useState<User[]>([]);
 
-  return (
-    <div></div>
-  )
+ useEffect(() => {
+  axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+  .then((res) => setUsers(res.data));
+ }, [])
+
+  return <ol className="list-group list-group-numbered">
+    {users.map(user => <li className="list-group-item" key={user.id}>{user.name}</li>)}
+  </ol>
 }
 
 export default App;
