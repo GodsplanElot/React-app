c0c8c55a3b070ed88a3b31821e2628b7d85c7fdf
Godsplan Elot <elotgodsplan2@gmail.com>
2025-08-24 04:53:38 +0100

    Modify Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import ListGroup from "./components/ListGroup";
+
 
 function App() {
-  return <div><ListGroup /></div>;
+  return (
+    <div>
+      <input type="text" className="form-control form-control-lg "/>
+    </div>
+  )
 }
 
 export default App;
